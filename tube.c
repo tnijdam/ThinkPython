f2364f5c3d4efa3d5e911ce2e97b99fde6d9a2b4
tnijdam <ta.nijdam@gmail.com>
2025-10-02 11:25:25 +0200

    Refactor variable names for clarity and improve formatting in tube.c

--- tube.c
+++ tube.c
@@ -12,13 +12,13 @@
 #define RADIAL (PI / 180)
 #define TIMES 180
 
-float tmp, y_start, x_start, x_end, brace1;
+float tmp, y_start, x_start, x_end, outside_radius_brace;
 
 int main(void)
 {
     float const rad = RADIAL;
     float pi, A, r1, r2, Alpha, Alpha_hlp1, Alpha_hlp2, Alpha_hlp3;
-    float od_leg, od_brace, wt_brace, angle, RadiusLeg, Rbrace, x_axis[TIMES], y_axis[TIMES], units;
+    float outside_diameter_leg, outside_diameter_brace, wall_thickness_brace, angle, outside_radius_leg, inside_radius_brace, x_axis[TIMES], y_axis[TIMES], units;
     int i;
     char unit;
 
@@ -26,11 +26,11 @@ int main(void)
     printf("Dimensions [M]etric of [I]mperial?\t: ");
     scanf("%s", &unit);
     printf("Outside diameter Leg\t\t: ");
-    scanf("%f", &od_leg);
+    scanf("%f", &outside_diameter_leg);
     printf("Outside diameter Brace\t\t: ");
-    scanf("%f", &od_brace);
+    scanf("%f", &outside_diameter_brace);
     printf("Wallthickness Brace\t\t: ");
-    scanf("%f", &wt_brace);
+    scanf("%f", &wall_thickness_brace);
     printf("Angle between Leg and Brace\t: ");
     scanf("%f", &angle);
 
@@ -48,15 +48,15 @@ int main(void)
     /*
      * Calculate the x-axis
      */
-    RadiusLeg = (od_leg / 2.0);
-    Rbrace = (od_brace - (2.0 * wt_brace)) / 2.0;
-    brace1 = (od_brace / 2.0);
+    outside_radius_leg = (outside_diameter_leg / 2.0);
+    inside_radius_brace = (outside_diameter_brace - (2.0 * wall_thickness_brace)) / 2.0;
+    outside_radius_brace = (outside_diameter_brace / 2.0);
     angle = angle * rad;
-    r1 = pow(Rbrace, 2);
-    r2 = pow(RadiusLeg, 2);
+    r1 = pow(inside_radius_brace, 2);
+    r2 = pow(outside_radius_leg, 2);
     // Debug info
-    printf("Radiusleg: %0.2lf, Rbrace: %0.2lf, brace1: %0.2lf, angle in rad: %lf\n", RadiusLeg,
-           Rbrace, brace1, angle);
+    printf("outside_radius_leg: %7.2lf, inside_radius_brace: %7.2lf, outside_radius_brace: %7.2lf, angle in rad: %lf\n", outside_radius_leg,
+           inside_radius_brace, outside_radius_brace, angle);
     for (i = 0; i <= 180; i += 10)
     {
         Alpha = i * 1.0;
@@ -65,8 +65,8 @@ int main(void)
         Alpha_hlp3 = cos(Alpha);
         Alpha_hlp2 = pow(Alpha_hlp1, 2);
         A = 1 - (sqrt(1 - ((r1 / r2) * Alpha_hlp2)));
-        x_axis[i] = ((RadiusLeg / sin(angle)) * A) + ((Rbrace / tan(angle)) * (1 - Alpha_hlp3));
-        y_axis[i] = fabs(brace1 * Alpha_hlp3);
+        x_axis[i] = ((outside_radius_leg / sin(angle)) * A) + ((inside_radius_brace / tan(angle)) * (1 - Alpha_hlp3));
+        y_axis[i] = fabs(outside_radius_brace * Alpha_hlp3);
         printf("x[%3i] = %8.3f, y[%3i] = %8.3f\n", i, x_axis[i], i, y_axis[i]);
     }
     system("pause");
